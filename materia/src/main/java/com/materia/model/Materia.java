d82c6875298555f14c5a30dc726fdc9ff3bd700b
josemalyson <josemalyson@josemalyson-laptop>
2016-11-26 02:29:34 -0300

    Project Rest Repository of Materials

---
+++ materia/src/main/java/com/materia/model/Materia.java
@@ -0,0 +1,84 @@
+package com.materia.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_MATERIA")
+public class Materia implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -905592428938819718L;
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "professor")
+	private String professor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((professor == null) ? 0 : professor.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Materia other = (Materia) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (professor == null) {
+			if (other.professor != null)
+				return false;
+		} else if (!professor.equals(other.professor))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Materia [id=" + id + ", nome=" + nome + ", professor=" + professor + "]";
+	}
+
+}
