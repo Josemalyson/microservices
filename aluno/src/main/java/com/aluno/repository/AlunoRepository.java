49ea467fa4b002157ff29eb67ba15f6e2587c385
josemalyson <josemalyson@gmail.com>
2016-12-16 08:46:08 -0300

    Correcting model of packages and layers of project.

--- aluno/src/main/java/com/aluno/repository/AlunoRepository.java
+++ aluno/src/main/java/com/aluno/repository/AlunoRepository.java
@@ -1,11 +1,9 @@
 package com.aluno.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.aluno.model.Aluno;
 
-@RepositoryRestResource(collectionResourceRel = "alunos", path="alunos")
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 
 }
